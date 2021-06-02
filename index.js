658e3b745ef1c8636f00a4fd222cb4e7202dd074
markusklassen1979 <markusklassen@gmail.com>
2021-06-02 14:48:53 +0200

    Solved Challenge 2

--- index.js
+++ index.js
@@ -18,6 +18,33 @@
  * Declare "x" and assign value 10 to it.
  * Declare "y" and assign value true to it.
  * Declare "myObject" and assign object with two name-value pairs.
- * 
+ * Declare "anotherObject".
+ * Later assign value to it - object with three name-value pairs
+ */
+let x = 10;
+const y = true;
+const myObject = {
+    a: x, 
+    b: y
+}; 
+console.log(myObject);
+/**
+ * {a: 10, b: true, __proto__: Object}
+ */
+x = 20;
+let anotherObject = {};
+anotherObject = {
+    newA: x, 
+    b: true, 
+    c: myObject
+};
+console.log(anotherObject);
+/**
+ * {
+ * newA: 20,
+ * b: true,
+ * c: {a:10, b:true, __proto__: Object},
+ * __proto__: Object
+ * }
  */
 
