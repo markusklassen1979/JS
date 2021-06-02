ab9347181064d41d305377a771cbeba0f7f481f1
markusklassen1979 <markusklassen@gmail.com>
2021-06-02 15:24:33 +0200

    Video 34

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
+let myVariable;
+
+console.log(myVariable); // undefined
+
+myVariable = 10;
+
+console.log(myVariable); // 10
+
+myVariable = true;
+
+console.log(myVariable); // true
+
+myVariable = {
+    x: true,
+    y: 10
+}
+
+console.log(myVariable); // {...}
+
+myVariable.y = 20;
+
+console.log(myVariable); // {...}
+
+myVariable = null;
+
+myVariable.x = false; // Uncaught TypeError: Cannot set property 'x' of null
+
+
 // /**
-//  * CHALLENGE 1
-//  * 
-//  * Declare variable "myObject" and assign value {}.
-//  * Print this variable to the console.
-//  * NOTE: variable "myObject" will NOT be reassigned in the future
+//  * CHALLENGE
 //  */
-
-// const myObject = {};
-
-// console.log(myObject);
-
-// // myObject = {}; // Uncaught TypeError: Assignment to constant variable.
-
-/**
- * CHALLENGE 2
- * 
- * Declare "x" and assign value 10 to it.
- * Declare "y" and assign value true to it.
- * Declare "myObject" and assign object with two name-value pairs.
- * Declare "anotherObject".
- * Later assign value to it - object with three name-value pairs
- */
-let x = 10;
-const y = true;
-const myObject = {
-    a: x, 
-    b: y
-}; 
-console.log(myObject);
-/**
- * {a: 10, b: true, __proto__: Object}
- */
-x = 20;
-let anotherObject;
-anotherObject = {
-    newA: x, 
-    b: true, 
-    c: myObject
-};
-console.log(anotherObject);
-/**
- * {
- * newA: 20,
- * b: true,
- * c: {a:10, b:true, __proto__: Object},
- * __proto__: Object
- * }
- */
-
+// let newVariable;
+// newVariable = null;
+// console.log(newVariable);
+// newVariable = 15;
+// console.log(newVariable);
+// newVariable = false;
+// console.log(newVariable);
