b7ae99461a7ee10fee17955b699779f635fdadf2
markusklassen1979 <markusklassen@gmail.com>
2021-06-02 14:12:55 +0200

    Video30

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello from external JavaScript file");
+// /**
+//  * Example 1 - Variable declaration
+//  */
+// var a;
+
+// console.log(a);
+
+// // console.log(b); //Uncaught ReferenceError: b is not defined
+
+// let c;
+
+// console.log(c);
+
+// //const d; //Uncaught SyntaxError: Missing initializer in const declaration
+
+
+// /**
+//  * Example 4 -"const"
+//  */
+// const myBoolean = true;
+
+// console.log(myBoolean);
+
+// // myBoolean = false; // Uncaught TypeError: Assignment to constant variable.
+
+// // const myBoolean = false; //Uncaught SyntaxError: Identifier 'myBoolean' has already been declared
