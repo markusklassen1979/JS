869267678b587e42d10626160120ae278082210d
markusklassen1979 <markusklassen@gmail.com>
2021-06-08 20:06:32 +0200

    Point 46

---
+++ index.js
@@ -0,0 +1,40 @@
+// /**
+//  * EXAMPLE 1
+//  * 
+//  * Simplest function in Javascripts
+//  */
+// function myFunction() {}
+
+// console.log(myFunction); // ƒ myFunction() {}
+
+// myFunction();
+
+// const functionResult = myFunction();
+
+// console.log(functionResult); // undefined
+
+// console.log(myFunction()); // undefined
+
+// console.log(myFunction(10, true)); // undefined
+
+// /**
+//  * EXAMPLE 2
+//  * 
+//  * Parameters of the function
+//  */
+
+// function myFunction(a, b) {
+//     console.log(a);
+//     console.log(b);
+// }
+
+// myFunction(10, 5); // 10, 5
+
+// myFunction(true, null); // true, null
+
+// myFunction(); // undefined, undefined
+
+// myFunction("abc"); // abc, undefined
+
+// console.log(myFunction(4,3)); // 4, 3, undefined
+
