68d926baad6797b06f13db6983dd4d00b72d43a1
markusklassen1979 <markusklassen@gmail.com>
2021-06-20 14:41:54 +0200

    After Video 57

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello from external JavaScript file");
+/**
+ * SECTION 1
+ * 
+ * Arithmetic Operators
+ */
+let a,b;
+a = 1;
+b = 2;
+
+// Math with numbers
+console.log(a + b); // 3
+console.log(a * b); // 2
+console.log(a / b); // 0.5
+console.log(a - b); // -1
+
+a = "abc";
+b = 5;
+
+// Math with non number
+console.log(a + b); // abc5
+console.log(a * b); // NaN
+console.log(a / b); // NaN
+console.log(a - b); // NaN
+
+a = "Hello";
+b = "World";
+// Strings concatenation
+console.log(a + " " + b);
