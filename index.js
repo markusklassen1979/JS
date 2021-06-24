4392cc365ae38f337ed2e2424b1c4a6b7bf130fc
markusklassen1979 <markusklassen@gmail.com>
2021-06-24 17:03:38 +0200

    Video 64

--- index.js
+++ index.js
@@ -221,3 +221,35 @@
 // console.log(5 * 10 / 2); // 25
 // console.log(5 * 10 / 2 / 5); // 5 left to right
 
+// /**
+//  * TASK 1
+//  * 
+//  * Compare 2 variables "myVariable1" and "myVariable2".
+//  * Log to the console "true" if "myVariable1" is less than or equal to myVariable2.
+//  * Convert both variables to numbers before comparison.
+//  */
+// let myVariable1 = 10;
+// let myVariable2 = "5";
+
+// console.log(+myVariable1 <= +myVariable2); // false
+
+// myVariable1 = "20";
+// myVariable2 = 100;
+
+// console.log(+myVariable1 <= +myVariable2); // true
+
+// /**
+//  * TASK 2
+//  * 
+//  * Print to the console remainder of the division of "myNumber1" by "myNumber2".
+//  * Which precedence and associativity has this operator?
+//  */
+// let myNumber1 = 10;
+// let myNumber2 = 3;
+
+// // Precedence 15 left to right
+// console.log(myNumber1 % myNumber2);
+// console.log(25 % 5); // 0
+// console.log(14 % 8); // 6
+// console.log(100 % 30 % 3); // 1
+
