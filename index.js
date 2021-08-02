8e01f5c84dfe043692c85f91986dfba57af4cf90
markusklassen1979 <markusklassen@gmail.com>
2021-08-02 06:49:26 +0200

    Video 69

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello from external JavaScript file");
+// /**
+//  * EXPRESSIONS
+//  */
+// 10
+// "abc"
+// 5 + 3
+
+// ;(function(a){
+//     console.log(a);
+// }) // Function expression embedded into the ()
+
+// // Immediately invoked function expression
+// ;(function(){
+//    console.log("Hello from Aachen"); 
+// })() // Function call is expression
+
+// let a;
+// a = 10 // Assignement expression
+
+// // 5 = true // index.js:20 Uncaught SyntaxError: Invalid left-hand side in assignment
+
+// 4 <= 10 <= 20 || 5 // true - Logical expression
+
