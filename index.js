dc9a2a60872b4636374c03c008c609415ea55ad8
markusklassen1979 <markusklassen@gmail.com>
2021-08-04 06:05:49 +0200

    Video 70

--- index.js
+++ index.js
@@ -21,3 +21,43 @@
 
 // 4 <= 10 <= 20 || 5 // true - Logical expression
 
+// /**
+//  * STATEMENTS
+//  */
+// let a; // Variable declaration is statement
+
+// a = 3
+
+// function myFunction(b){
+//     // Quick check that return is a statement
+//     //console.log(return b);
+//     return b;
+// }
+
+// myFunction(a)
+
+// if (true){
+//     console.log("Hello from 'if' statement!");
+// } // "if" statement
+
+// // Quick check that if is statement
+// // console.log(if (true){
+// //     console.log("Hello from 'if' statement!");
+// // }); // Uncaught SyntaxError: Unexpected token 'if'
+
+// /**
+//  * EXPRESSION STATEMENTS
+//  */
+// console.log(4 + 3); // Experrsion statement
+
+// //console.log(console.log(4 + 3););//Uncaught SyntaxError: missing ) after argument list 
+
+// console.log(console.log(4 + 3)); // Experrsion statement
+
+// let a;
+// a = 5; // Expession statement
+
+// true;
+
+// //console.log(true;); // Uncaught SyntaxError: missing ) after argument list
+
